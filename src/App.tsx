cbae8a211d8f60b225d95db081f570336d07752e
piradiusquared <kpan5121@gmail.com>
2026-06-23 21:26:21 +1000

    Added React-Router-DOM into the app, testing with basic navbar.

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,42 @@
+// Package imports
+import { Routes, Route, Link } from 'react-router-dom'
+
+// CSS imports
 import './App.css'
 
 function App() {
   
   return (
     <>
-      <section id="main">
-        GURTS
-      </section>
+      <nav style={{ padding: '20px', borderBottom: '1px solid #ccc' }}> {/* Remove inline styling */}
+        <Link to="/" style={{ marginRight: '10px' }}>Overview</Link>
+        <Link to="/owners">Owners</Link>
+      </nav>
+
+      {/* Routing */}
+      <Routes>
+        {/* Actual routes */}
+        <Route path="/" element={<Dashboard />} />
+        <Route path="/owners" element={<Owners />} />
+      </Routes>
     </>
+  )
+}
+
+// Routing Functions
+function Dashboard() {
+  return (
+    <div>
+      <h1>Dashboard oh yeah</h1>
+    </div>
+  )
+}
+
+function Owners() {
+  return (
+    <div>
+      <h1>Owners page and stuff</h1>
+    </div>
   )
 }
 
