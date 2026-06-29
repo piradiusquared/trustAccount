1c060e3291b1eacf2f14b6785470ce72e7c1e40d
piradiusquared <kpan5121@gmail.com>
2026-06-29 10:41:02 +1000

    Added in new routing for subpage

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@
 import { Routes, Route, Link } from 'react-router-dom'
 
 // Modular tsx files
-import Owners from './pages/pages-tsx/owners'
+import { Owners, NewOwner } from './pages/pages-tsx/owners'
 import Dashboard from './pages/pages-tsx/dashboard'
 import Properties from './pages/pages-tsx/properties'
 import Leases from './pages/pages-tsx/leases'
@@ -14,13 +14,13 @@ import './app.css'
 function App() {
   return (
     <>
-      <div className="topnav-container">
-        <nav className="topnav"> {/* Remove inline styling */}
-          <Link className='toplink' to="/">Overview</Link>
-          <Link className='toplink' to="/owners">Owners</Link>
-          <Link className='toplink' to="/properties">Properties</Link>
-          <Link className='toplink' to="/leases">Leases</Link>
-          <Link className='toplink' to="/testdb">Test DB</Link>
+      <div className='topnav-container'>
+        <nav className='topnav'> {/* Remove inline styling */}
+          <Link className='toplink' to='/'>Overview</Link>
+          <Link className='toplink' to='/owners'>Owners</Link>
+          <Link className='toplink' to='/properties'>Properties</Link>
+          <Link className='toplink' to='/leases'>Leases</Link>
+          <Link className='toplink' to='/testdb'>Test DB</Link>
         </nav>
       </div>
 
@@ -28,11 +28,23 @@ function App() {
       {/* Routing */}
       <Routes>
         {/* Actual routes */}
-        <Route path="/" element={<Dashboard />} />
-        <Route path="/owners" element={<Owners />} />
-        <Route path="/properties" element={<Properties />} />
-        <Route path="/leases" element={<Leases />} />
-        <Route path="/testdb" element={<TestDb />} />
+        {/* Root */}
+        <Route path='/' element={<Dashboard />} />
+
+        {/* Owners */}
+        <Route path='/owners' element={<Owners />} />
+        <Route path='/owners/add-owner' element={<NewOwner />} />
+
+        {/* Properties */}
+        <Route path='/properties' element={<Properties />} />
+
+        {/* Leases */}
+        <Route path='/leases' element={<Leases />} />
+
+
+        <Route path='/testdb' element={<TestDb />} />
+
+
       </Routes>
     </>
   )
