5dd57ce63574b59f9e9a2dc57c0cba11882d65c2
piradiusquared <kpan5121@gmail.com>
2026-06-29 11:16:08 +1000

    Added routing in app.tsx for subpages. Removed unnecessary "side" navigation container

--- src/App.tsx
+++ src/App.tsx
@@ -4,8 +4,8 @@ import { Routes, Route, Link } from 'react-router-dom'
 // Modular tsx files
 import { Owners, NewOwner } from './pages/pages-tsx/owners'
 import Dashboard from './pages/pages-tsx/dashboard'
-import Properties from './pages/pages-tsx/properties'
-import Leases from './pages/pages-tsx/leases'
+import { Properties, NewProperty} from './pages/pages-tsx/properties'
+import { Leases, NewLease } from './pages/pages-tsx/leases'
 import TestDb from './pages/pages-tsx/test-db'
 
 // CSS imports
@@ -14,15 +14,13 @@ import './app.css'
 function App() {
   return (
     <>
-      <div className='topnav-container'>
-        <nav className='topnav'> {/* Remove inline styling */}
-          <Link className='toplink' to='/'>Overview</Link>
-          <Link className='toplink' to='/owners'>Owners</Link>
-          <Link className='toplink' to='/properties'>Properties</Link>
-          <Link className='toplink' to='/leases'>Leases</Link>
-          <Link className='toplink' to='/testdb'>Test DB</Link>
-        </nav>
-      </div>
+      <nav className='topnav'> {/* Remove inline styling */}
+        <Link className='toplink' to='/'>Overview</Link>
+        <Link className='toplink' to='/owners'>Owners</Link>
+        <Link className='toplink' to='/properties'>Properties</Link>
+        <Link className='toplink' to='/leases'>Leases</Link>
+        <Link className='toplink' to='/testdb'>Test DB</Link>
+      </nav>
 
 
       {/* Routing */}
@@ -33,14 +31,15 @@ function App() {
 
         {/* Owners */}
         <Route path='/owners' element={<Owners />} />
-        <Route path='/owners/add-owner' element={<NewOwner />} />
+        <Route path='/owners/new-owner' element={<NewOwner />} />
 
         {/* Properties */}
         <Route path='/properties' element={<Properties />} />
+        <Route path='/properties/new-property' element={<NewProperty />} />
 
         {/* Leases */}
         <Route path='/leases' element={<Leases />} />
-
+        <Route path='/leases/new-lease' element={<NewLease />} />
 
         <Route path='/testdb' element={<TestDb />} />
 
